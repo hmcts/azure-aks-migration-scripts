627270dc2daa82057cd8123b6fd11c3d9510fe51
Tim Jacomb <tim.jacomb@hmcts.net>
2019-10-02 11:08:21 +0100

    Fast fallback scripts to ASE

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # azure-aks-migration-scripts
 Scripts to help in the AKS migration
+
+## DNS switch over 
+
+To run these scripts you will need network connectivity to the consul servers for the environment you are switching over, (Currently supported environments are `aat` and `prod`).
+
+You will need to be on the VPN,
+and either on `bastion.reform` or using `sshuttle -r bastion.reform 10.0.0.0/8` for the network access.
+
+### dns-switch-back-to-ase.sh
+In the unlikely event that we need to switch back to the ASE you can run this script to do it.
+```bash
+./dns-switch-back-to-ase.sh plum-recipes-service prod
+```
+
+### dns-switch-to-aks.sh
+If you want to do a quick DNS switch you can use this script, or accidentally change the wrong app to ASE you can use this script:
+```bash
+./dns-switch-to-aks.sh plum-recipes-service prod
+```
---
+++ dns-switch-back-to-ase.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+service=$1
+env=$2
+
+serviceName=${service}-${env}
+
+function usage() {
+  echo "usage: ./dns-switch-back-to-ase.sh <service, i.e cmc-claim-store> <env, prod or aat>" 
+}
+
+if [ -z "${service}" ] || [ -z "${env}" ] ; then
+  usage
+  exit 1
+fi
+
+domain=${service}-${env}.service.core-compute-${env}.internal
+ipAddress=""
+
+case "${env}" in
+  "aat")
+    SWITCH_ADDRESS=10.96.140.11
+    CONSUL_NODES=(10.96.136.7 10.96.136.9 10.96.136.10)
+  ;;
+  "prod")
+    SWITCH_ADDRESS=10.96.76.11
+    CONSUL_NODES=(10.96.72.4 10.96.72.5 10.96.72.6)
+esac
+
+echo "Changing DNS entry to point to ASE in ${env} for ${domain}"
+
+echo "DNS is currently pointing to:"
+dig +short $domain
+
+set -euo pipefail
+
+for nodeIP in ${CONSUL_NODES[*]}
+do
+  curl -X PUT -H "Accept: application/json" http://${nodeIP}:8500/v1/agent/service/deregister/$service-$env  || echo "Failed reregistering from $nodeIP"
+done
+
+curl -X PUT -H "Accept: application/json" http://${CONSUL_NODES[0]}:8500/v1/agent/service/register -d '
+{
+  "ID": "'${serviceName}'",
+  "Name": "'${serviceName}'",
+  "Service": "'${serviceName}'",
+  "Address": "'${SWITCH_ADDRESS}'",
+  "Port": 80
+}
+'
+
+sleep 5
+echo "DNS is currently pointing to:"
+dig +short $domain
+
+echo "Switch complete"
+
+echo
+echo ' __      __                     .__                
+/  \    /  \_____ _______  ____ |__| ____    ____  
+\   \/\/   /\__  \\_  __ \/    \|  |/    \  / ___\ 
+ \        /  / __ \|  | \/   |  \  |   |  \/ /_/  >
+  \__/\  /  (____  /__|  |___|  /__|___|  /\___  / 
+       \/        \/           \/        \//_____/  '
+
+echo "You now need to make sure the disableLegacyDeployment() flag is removed from the teams Jenkinsfile_CNP"
+echo "Otherwise Jenkins will revert this change"
