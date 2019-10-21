873772035fb89aa68e14452b20b5c22d6eca2979
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-10-21 11:15:35 +0100

    Refactor script

--- check-prod-health-host.sh
+++ check-prod-health-host.sh
@@ -1,6 +1,29 @@
-HOST=${1}.service.core-compute-prod.internal
-curl -H "Host: ${HOST}" http://10.13.15.250/health
-echo
-curl -H "Host: ${HOST}" http://10.13.31.250/health
-echo
-curl -H "Host: ${HOST}" http://10.13.32.120/health
+#!/usr/bin/env bash
+
+IPS=( "10.13.15.250"
+  "10.13.31.250"
+  "10.13.32.120"
+)
+
+HOST_NAME="$1"
+
+function usage() {
+  echo "usage: ./check-prod-health-host.sh  <host-name>"
+}
+
+if [ -z "${HOST_NAME}" ]
+then
+  usage
+  exit 1
+fi
+
+_len=$(expr ${#IPS[@]} - 1)
+
+HOST=${HOST_NAME}.service.core-compute-prod.internal
+
+for i in $(seq 0 ${_len})
+do
+  echo "Host: ${HOST} -> http://${IPS[${i}]}/health"
+  curl -H "Host: ${HOST}" http://${IPS[${i}]}/health
+  echo
+done
